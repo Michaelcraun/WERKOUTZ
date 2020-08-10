95f75d3572557355d91175872a7c5849870a5577
Michael Craun <michael.craun@gmail.com>
2020-08-10 13:44:12 -0400

    Initial Commit

---
+++ WERKOUTZ/WERKOUTZApp.swift
@@ -0,0 +1,17 @@
+//
+//  WERKOUTZApp.swift
+//  WERKOUTZ
+//
+//  Created by Michael Craun on 8/10/20.
+//
+
+import SwiftUI
+
+@main
+struct WERKOUTZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
